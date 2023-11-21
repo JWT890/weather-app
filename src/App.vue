65585238f26783b669b8f1bcd3f00eebe9b7edb7
Jon William Taylor <jwtaylor659@gmail.com>
2023-11-21 11:58:37 -0600

    Weather app using Open Weather API

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+    <div id="app">
+      <Weatherapp />
+    </div>
+  </template>
+  
+  <script>
+  import Weatherapp from './components/Weatherapp.vue'
+  
+  export default {
+    components: {
+      Weatherapp
+    }
+  }
+  </script>
